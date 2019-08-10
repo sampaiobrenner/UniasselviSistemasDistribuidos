8cfeebe8146c35d95093f1e0cdb6c85886ddf388
Matheus Sampaio <sampaio.brenner@gmail.com>
2019-08-10 01:45:50 -0300

    Add project files.

---
+++ Client/Program.cs
@@ -0,0 +1,87 @@
+﻿// A C# program for Client
+
+using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace Client
+{
+    public class Program
+    {
+        // Main Method
+        private static void Main(string[] args)
+        {
+            ExecuteClient();
+        }
+
+        // ExecuteClient() Method
+        private static void ExecuteClient()
+        {
+            try
+            {
+                // Establish the remote endpoint
+                // for the socket. This example
+                // uses port 11111 on the local
+                // computer.
+                var ipHost = Dns.GetHostEntry(Dns.GetHostName());
+                var ipAddr = ipHost.AddressList[0];
+                var localEndPoint = new IPEndPoint(ipAddr, 11111);
+
+                // Creation TCP/IP Socket using
+                // Socket Class Costructor
+                var sender = new Socket(ipAddr.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+
+                try
+                {
+                    // Connect Socket to the remote
+                    // endpoint using method Connect()
+                    sender.Connect(localEndPoint);
+
+                    // We print EndPoint information
+                    // that we are connected
+                    Console.WriteLine("Socket connected to -> {0} ", sender.RemoteEndPoint);
+
+                    // Creation of messagge that
+                    // we will send to Server
+                    var messageSent = Encoding.UTF8.GetBytes("Test Client<EOF>");
+                    var byteSent = sender.Send(messageSent);
+
+                    // Data buffer
+                    var messageReceived = new byte[1024];
+
+                    // We receive the messagge using
+                    // the method Receive(). This
+                    // method returns number of bytes
+                    // received, that we'll use to
+                    // convert them to string
+                    var byteRecv = sender.Receive(messageReceived);
+                    Console.WriteLine("Message from Server -> {0}", Encoding.UTF8.GetString(messageReceived, 0, byteRecv));
+
+                    // Close Socket using
+                    // the method Close()
+                    sender.Shutdown(SocketShutdown.Both);
+                    sender.Close();
+                }
+
+                // Manage of Socket's Exceptions
+                catch (ArgumentNullException ane)
+                {
+                    Console.WriteLine("ArgumentNullException : {0}", ane);
+                }
+                catch (SocketException se)
+                {
+                    Console.WriteLine("SocketException : {0}", se);
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("Unexpected exception : {0}", e);
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+        }
+    }
+}
---
+++ Server/Program.cs
@@ -0,0 +1,91 @@
+﻿// A C# Program for Server
+using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace Server
+{
+    internal class Program
+    {
+        // Main Method
+        private static void Main(string[] args)
+        {
+            ExecuteServer();
+        }
+
+        public static void ExecuteServer()
+        {
+            // Establish the local endpoint
+            // for the socket. Dns.GetHostName
+            // returns the name of the host
+            // running the application.
+            var ipHost = Dns.GetHostEntry(Dns.GetHostName());
+            var ipAddr = ipHost.AddressList[0];
+            var localEndPoint = new IPEndPoint(ipAddr, 11111);
+
+            // Creation TCP/IP Socket using
+            // Socket Class Costructor
+            var listener = new Socket(ipAddr.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+
+            try
+            {
+                // Using Bind() method we associate a
+                // network address to the Server Socket
+                // All client that will connect to this
+                // Server Socket must know this network
+                // Address
+                listener.Bind(localEndPoint);
+
+                // Using Listen() method we create
+                // the Client list that will want
+                // to connect to Server
+                listener.Listen(10);
+
+                while (true)
+                {
+                    Console.WriteLine("Waiting connection ... ");
+
+                    // Suspend while waiting for
+                    // incoming connection Using
+                    // Accept() method the server
+                    // will accept connection of client
+                    var clientSocket = listener.Accept();
+
+                    // Data buffer
+                    var bytes = new Byte[1024];
+                    string data = null;
+
+                    while (true)
+                    {
+                        var numByte = clientSocket.Receive(bytes);
+
+                        data += Encoding.UTF8.GetString(bytes,
+                                                0, numByte);
+
+                        if (data.IndexOf("<EOF>") > -1)
+                            break;
+                    }
+
+                    Console.WriteLine("Text received -> {0} ", data);
+                    var message = Encoding.UTF8.GetBytes("Test Server");
+
+                    // Send a message to Client
+                    // using Send() method
+                    clientSocket.Send(message);
+
+                    // Close client Socket using the
+                    // Close() method. After closing,
+                    // we can use the closed Socket
+                    // for a new Client Connection
+                    clientSocket.Shutdown(SocketShutdown.Both);
+                    clientSocket.Close();
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+        }
+    }
+}
